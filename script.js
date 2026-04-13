13a3e01a26593c24d1b7d6ba39d2ebd59159e904
guptaroshni-25 <137182638+guptaroshni-25@users.noreply.github.com>
2026-04-13 11:02:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,18 +3,35 @@ function exploreBooks() {
 }
 
 function viewProduct() {
+  dataLayer.push({
+    event: "view_item"
+  });
+
   window.location.href = "product-detail.html";
 }
 
 function addToCart() {
+  dataLayer.push({
+    event: "add_to_cart"
+  });
+
   alert("Added to cart");
 }
 
 function buyNow() {
+  dataLayer.push({
+    event: "purchase"
+  });
+
   alert("Purchase initiated");
 }
 
 function submitForm(event) {
   event.preventDefault();
+
+  dataLayer.push({
+    event: "form_submit"
+  });
+
   alert("Form submitted");
 }
